140ab7eda7971078e0605811b4954c6fad35c92a
Samuel Kulisek <xkulise3@fi.muni.cz>
2021-05-20 23:54:12 +0200

    Add CLI switches to enable replacing spaces and inclusion of positive influences

--- sbgn2bcsl.py
+++ sbgn2bcsl.py
@@ -26,24 +26,37 @@ arg_parser.add_argument('-C',
                         '--unpack-nested-complexes',
                         action='store_true',
                         help='Enable the unpacking of nested complexes.')
+arg_parser.add_argument('-i',
+                        '--include-positive-influences',
+                        action='store_true',
+                        help='Include positive influences on both sides of the rule.')
+arg_parser.add_argument('-w',
+                        '--replace-spaces',
+                        action='store_true',
+                        help='Replace whitespaces in names with an underscore.')
 
 args = arg_parser.parse_args()
 input_path = args.Path
 output_path = args.Out_path
 unpack_complexes = args.unpack_complexes
 unpack_nested = args.unpack_nested_complexes
+include_influences = args.include_positive_influences
+quiet = args.quiet
+replace_spaces = args.replace_spaces
 
 if unpack_nested:
     unpack_complexes = True
 
 cp = CellDesignerParser(input_path)
-cp.generate_warnings = not args.quiet
+cp.generate_warnings = not quiet
 cp.parse_tree()
 
 tsr = Translator()
 tsr.unpack_complexes = unpack_complexes
 tsr.unpack_nested_complexes = unpack_nested
-tsr.generate_warnings = not args.quiet
+tsr.generate_warnings = not quiet
+tsr.include_positive_influences = include_influences
+tsr.replace_spaces = replace_spaces
 
 rules_list = []
 for process in cp.process_list:
